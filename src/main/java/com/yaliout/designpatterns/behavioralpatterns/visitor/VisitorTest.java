dfd97b1b8eba65b1a697c5a45d15fbe48b947877
遥云挽月 <59269619+mistytulip@users.noreply.github.com>
2020-11-09 20:33:55 +0800

    commit: 2020/11/09 访问者模式

---
+++ src/main/java/com/yaliout/designpatterns/behavioralpatterns/visitor/VisitorTest.java
@@ -0,0 +1,12 @@
+package com.yaliout.designpatterns.behavioralpatterns.visitor;
+
+/**
+ * 访问者模式：
+ * 在被访问的类里面加一个对外提供接待访问者的接口。
+ *
+ * @author Hu JunJie
+ * @date 2020/11/9 20:23
+ * @since
+ */
+public class VisitorTest {
+}
